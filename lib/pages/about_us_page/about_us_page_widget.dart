c111a93f9e27f19fe503d1af273a6e06e73a1016
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-26 02:33:39 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/about_us_page/about_us_page_widget.dart
+++ lib/pages/about_us_page/about_us_page_widget.dart
@@ -36,7 +36,7 @@ class _AboutUsPageWidgetState extends State<AboutUsPageWidget> {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        backgroundColor: Colors.white,
         body: SafeArea(
           top: true,
           child: Column(
@@ -48,11 +48,11 @@ class _AboutUsPageWidgetState extends State<AboutUsPageWidget> {
                 children: [
                   FlutterFlowIconButton(
                     borderRadius: 8.0,
-                    buttonSize: 40.0,
+                    buttonSize: 50.0,
                     icon: const Icon(
                       Icons.chevron_left,
                       color: Color(0xFFB1B1B1),
-                      size: 24.0,
+                      size: 28.0,
                     ),
                     onPressed: () async {
                       context.safePop();
@@ -65,7 +65,10 @@ class _AboutUsPageWidgetState extends State<AboutUsPageWidget> {
                         'About Us',
                         style: FlutterFlowTheme.of(context).bodyMedium.override(
                               fontFamily: 'Inter',
+                              color: const Color(0xFF333333),
+                              fontSize: 20.0,
                               letterSpacing: 0.0,
+                              fontWeight: FontWeight.w600,
                             ),
                       ),
                     ),
@@ -73,8 +76,8 @@ class _AboutUsPageWidgetState extends State<AboutUsPageWidget> {
                   Opacity(
                     opacity: 0.0,
                     child: Container(
-                      width: 40.0,
-                      height: 40.0,
+                      width: 50.0,
+                      height: 50.0,
                       decoration: BoxDecoration(
                         color: FlutterFlowTheme.of(context).secondaryBackground,
                       ),
@@ -85,40 +88,44 @@ class _AboutUsPageWidgetState extends State<AboutUsPageWidget> {
               Expanded(
                 child: Align(
                   alignment: const AlignmentDirectional(0.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Align(
-                        alignment: const AlignmentDirectional(0.0, 0.0),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.asset(
-                            'assets/images/Group_3811@2x.png',
-                            width: MediaQuery.sizeOf(context).width * 0.4,
-                            fit: BoxFit.cover,
-                            alignment: const Alignment(0.0, 0.0),
+                  child: Padding(
+                    padding:
+                        const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 150.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Align(
+                          alignment: const AlignmentDirectional(0.0, 0.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.asset(
+                              'assets/images/Group_3811@2x.png',
+                              width: MediaQuery.sizeOf(context).width * 0.4,
+                              fit: BoxFit.cover,
+                              alignment: const Alignment(0.0, 0.0),
+                            ),
                           ),
                         ),
-                      ),
-                      Align(
-                        alignment: const AlignmentDirectional(0.0, 0.0),
-                        child: Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              0.0, 20.0, 0.0, 0.0),
-                          child: Text(
-                            'V 1.0.0',
-                            style: FlutterFlowTheme.of(context)
-                                .bodyMedium
-                                .override(
-                                  fontFamily: 'HouDiHei',
-                                  letterSpacing: 0.0,
-                                  useGoogleFonts: false,
-                                ),
+                        Align(
+                          alignment: const AlignmentDirectional(0.0, 0.0),
+                          child: Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                0.0, 20.0, 0.0, 0.0),
+                            child: Text(
+                              'V 1.0.0',
+                              style: FlutterFlowTheme.of(context)
+                                  .bodyMedium
+                                  .override(
+                                    fontFamily: 'HouDiHei',
+                                    letterSpacing: 0.0,
+                                    useGoogleFonts: false,
+                                  ),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
